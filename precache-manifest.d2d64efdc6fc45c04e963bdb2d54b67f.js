f82ca852b4376bf8469c93d6e833f51528475666
daniel seoh <djseoh@gmail.com>
2020-03-02 15:35:23 -0500

    Updates

---
+++ precache-manifest.d2d64efdc6fc45c04e963bdb2d54b67f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3978d71ba198c58caf1136cb91f06cc",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "8c73649413d62ef9619e",
+    "url": "/todolist/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "f669691ceab847a2785f",
+    "url": "/todolist/static/js/2.8d13b544.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todolist/static/js/2.8d13b544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c73649413d62ef9619e",
+    "url": "/todolist/static/js/main.aa1d8fb5.chunk.js"
+  },
+  {
+    "revision": "cebccde0f7f76a903cc9",
+    "url": "/todolist/static/js/runtime-main.6fa7f924.js"
+  }
+]);
